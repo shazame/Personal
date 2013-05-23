959fb2400a9639a2d2d4c6b5ad5d7c1699636feb
David <david.bitonneau@gmail.com>
2013-05-23 16:06:49 +0200

    Adding awesome widgets: mem, bat, cpu, volume

--- config/rc.lua
+++ config/rc.lua
@@ -10,6 +10,7 @@ local beautiful = require("beautiful")
 -- Notification library
 local naughty = require("naughty")
 local menubar = require("menubar")
+local vicious = require("vicious")
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -162,6 +163,45 @@ mytasklist.buttons = awful.util.table.join(
                                               if client.focus then client.focus:raise() end
                                           end))
 
+ -- START OF WIDGET ADDITION
+ 
+ -- Initialize widget
+ memwidget = awful.widget.progressbar()
+ -- Progressbar properties
+ memwidget:set_width(8)
+ memwidget:set_height(10)
+ memwidget:set_vertical(true)
+ memwidget:set_background_color("#494B4F")
+ memwidget:set_border_color(nil)
+ memwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#AECF96"}, {0.5, "#88A175"}, 
+                     {1, "#FF5656"}}})
+ -- Register widget
+ vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
+ 
+ 
+ -- Initialize widget
+ cpuwidget = awful.widget.graph()
+ -- Graph properties
+ cpuwidget:set_width(50)
+ cpuwidget:set_background_color("#494B4F")
+ cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
+                     {1, "#AECF96" }}})
+ -- Register widget
+ vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+
+
+ -- Initialize widget
+ batwidget = wibox.widget.textbox()
+ -- Register widget
+ vicious.register(batwidget, vicious.widgets.bat, "$2% $3", 60, "BAT0")
+
+ -- Initialize widget
+ volwidget = wibox.widget.textbox()
+ -- Register widget
+ vicious.register(volwidget, vicious.widgets.volume, "$1 $2", 2, "Master")
+ 
+ -- END OF WIDGET ADDITION
+
 for s = 1, screen.count() do
     -- Create a promptbox for each screen
     mypromptbox[s] = awful.widget.prompt()
@@ -191,6 +231,10 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
+    right_layout:add(volwidget)
+    right_layout:add(cpuwidget)
+    right_layout:add(batwidget)
+    right_layout:add(memwidget)
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
